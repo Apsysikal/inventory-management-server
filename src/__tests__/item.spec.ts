36d685d669a7f1b843e79b26620df40990e13b14
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2022-03-22 12:58:04 +0100

    Better readability

--- src/__tests__/item.spec.ts
+++ src/__tests__/item.spec.ts
@@ -10,6 +10,7 @@ describe("Tests the /item endpoint", () => {
 
       expect(response.statusCode).toStrictEqual(200);
     });
+
     it("Should return a status code 400 when called with ?skip={non-number}", async () => {
       const response = await request(app).get("/item?skip=abc").send();
 
@@ -17,6 +18,7 @@ describe("Tests the /item endpoint", () => {
       expect(response.type).toStrictEqual("application/json");
       expect(response.body).toContain("Bad input parameter");
     });
+
     it("Should return a status code 400 when called with ?skip={less-than-zero}", async () => {
       const response = await request(app).get("/item?skip=-1").send();
 
@@ -24,6 +26,7 @@ describe("Tests the /item endpoint", () => {
       expect(response.type).toStrictEqual("application/json");
       expect(response.body).toContain("Bad input parameter");
     });
+
     it("Should return a status code 400 when called with ?limit={non-number}", async () => {
       const response = await request(app).get("/item?limit=abc").send();
 
@@ -31,6 +34,7 @@ describe("Tests the /item endpoint", () => {
       expect(response.type).toStrictEqual("application/json");
       expect(response.body).toContain("Bad input parameter");
     });
+
     it("Should return a status code 400 when called with ?limit={less-than-zero}", async () => {
       const response = await request(app).get("/item?limit=-1").send();
 
@@ -38,6 +42,7 @@ describe("Tests the /item endpoint", () => {
       expect(response.type).toStrictEqual("application/json");
       expect(response.body).toContain("Bad input parameter");
     });
+
     it("Should return a status code 400 when called with ?limit={greater-than-max}", async () => {
       const response = await request(app).get("/item?limit=75").send();
 
@@ -45,23 +50,29 @@ describe("Tests the /item endpoint", () => {
       expect(response.type).toStrictEqual("application/json");
       expect(response.body).toContain("Bad input parameter");
     });
+
     // Return values, skip parameter
     it("Should return the first 25 items of the database when called with ?skip not defined", async () => {
       throw new Error("Test not defined");
     });
+
     it("Should return the first 25 items of the database when called with ?skip=0", async () => {
       throw new Error("Test not defined");
     });
+
     it("Should return the items 26-50 inclusive of the database when called with ?skip=25", async () => {
       throw new Error("Test not defined");
     });
+
     // Return values, limit parameter
     it("Should return a maximum of 25 items of the database when called with ?limit not defined", async () => {
       throw new Error("Test not defined");
     });
+
     it("Should return a maximum of 25 items of the database when called with ?limit=25", async () => {
       throw new Error("Test not defined");
     });
+
     it("Should return a maximum of 50 items of the database when called with ?limit=50", async () => {
       throw new Error("Test not defined");
     });
