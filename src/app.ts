9865d2809b7842c40b24ac285db9aeb04afa845a
Benedikt Schniepp <schnieppbenedikt@icloud.com>
2022-04-08 12:37:12 +0000

    Added morgan

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,13 @@
 import path from "path";
 import express from "express";
 import cors from "cors";
-
+import morganMiddleware from "./middlewares/morganMiddleware";
 import itemRouter from "./routes/item.route";
 
 const app = express();
 
 app.use(cors());
+app.use(morganMiddleware);
 
 app.use(express.static(path.resolve(__dirname, "..", "public")));
 app.use(express.json());
---
+++ src/middlewares/morganMiddleware.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from "express";
+import morgan, { StreamOptions } from "morgan";
+import Logger from "../config/logger";
+
+const stream: StreamOptions = {
+  write: (message: string) => Logger.debug(message),
+};
+
+const skip = () => {
+  return process.env.NODE_ENV === "development" ? false : true;
+};
+
+const options: morgan.Options<Request, Response> = {
+  skip,
+  stream,
+};
+
+const morganMiddleware = morgan(
+  ":method :url :status :res[content-length] - :response-time ms",
+  options
+);
+
+export default morganMiddleware;
